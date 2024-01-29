1be7a54effee823ca1d6593486e823274910cd1b
Pierpaolo Colasante <colasantepierpaolo@gmail.com>
2024-01-29 10:05:53 +0100

    create project

---
+++ src/main/java/buld/week/u5w4bw/U5w4bwApplication.java
@@ -0,0 +1,13 @@
+package buld.week.u5w4bw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w4bwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w4bwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/buld/week/u5w4bw/U5w4bwApplicationTests.java
@@ -0,0 +1,13 @@
+package buld.week.u5w4bw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w4bwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
