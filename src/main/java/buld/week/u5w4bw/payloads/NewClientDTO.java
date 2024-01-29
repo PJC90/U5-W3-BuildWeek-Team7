b64aa5dd624a90298dd0bb948ca9a6b90148edae
Angela Prestano <angiprestano99@gmail.com>
2024-01-29 16:42:39 +0100

    change entities

--- src/main/java/buld/week/u5w4bw/payloads/NewClientDTO.java
+++ src/main/java/buld/week/u5w4bw/payloads/NewClientDTO.java
@@ -1,7 +1,5 @@
 package buld.week.u5w4bw.payloads;
-
-
-import buld.week.u5w4bw.Entities.enums.BusinessType;
+import buld.week.u5w4bw.entities.enums.BusinessType;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
